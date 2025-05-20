4a4e75d7dc84659dec6f70c8934b92bb22c6a7df
mdecycu <mdecycuorg@gmail.com>
2025-05-20 10:43:00 +0800

    update exam2 world and related files

---
+++ downloads/webots_files/fourbar_slvs_exam2/controllers/fourbar_controller1/fourbar_controller1.py
@@ -0,0 +1,30 @@
+from controller import Robot
+
+def run_robot():
+    # Create the Robot instance
+    robot = Robot()
+
+    # Get simulation time step
+    timestep = int(robot.getBasicTimeStep())
+
+    # Get motor device
+    motor1 = robot.getDevice('joint_motor1')
+    #motor2 = robot.getDevice('joint_motor2')
+    #motor3 = robot.getDevice('joint_motor3')
+
+    # Set motor for continuous rotation
+    motor1.setPosition(float('inf'))
+    motor1.setVelocity(1.0)
+    """
+    motor2.setPosition(float('inf'))
+    motor2.setVelocity(1.0)
+    motor3.setPosition(float('inf'))
+    motor3.setVelocity(1.0)
+    """
+
+    # Main control loop
+    while robot.step(timestep) != -1:
+        pass
+
+if __name__ == "__main__":
+    run_robot()
