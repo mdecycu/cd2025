9c0d746a951705c2be1e3c510f76738bd35d98fe
mdecycu <mdecycuorg@gmail.com>
2025-06-13 15:30:39 +0800

    update w18 two carts tracking example

--- init.py
+++ init.py
@@ -11,7 +11,8 @@ class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
     site_title = "cd2025"
-    ip = "127.0.0.1"
+    #ip = "127.0.0.1"
+    ip = "2001:288:6004:17:10ff::2"
     dynamic_port = 9448
     static_port = 8448
     def __init__(self):
