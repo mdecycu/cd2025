782ad2701bad500ba062cbfb67390d0bfbe8ef62
mdecycu <mdecycuorg@gmail.com>
2025-06-16 11:27:47 +0800

    加入各 Homework 製作過程中相關 Tutorial 的提問問題組

--- init.py
+++ init.py
@@ -11,8 +11,7 @@ class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
     site_title = "cd2025"
-    #ip = "127.0.0.1"
-    ip = "2001:288:6004:17:10ff::2"
+    ip = "127.0.0.1"
     dynamic_port = 9448
     static_port = 8448
     def __init__(self):
